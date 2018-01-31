b9d9075373fc959561b77335ccf25a3fd1254dac
Michael Mammoliti <mammoliti.michael@gmail.com>
2018-01-31 13:20:07 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const sortObject = obj => {
+  const keys = Object.keys(obj).sort();
+
+  return keys
+    .reduce((acc, key) => {
+      let value = obj[key];
+
+      // it's an object!
+      if (Object(value) === value) {
+        value = orderObject(value);
+      }
+
+      return { ...acc, [key]: value }
+    }, {})
+  ;
+}
+
+export default sortObject;
